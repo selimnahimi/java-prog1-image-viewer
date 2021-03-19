72460e8c9b276627f5508a48e7257f8cb4173de0
Fajka0 <79834205+Fajka0@users.noreply.github.com>
2021-03-19 11:41:35 +0100

    asdasd

--- src/com/prog1/kepnezegeto/Main.java
+++ src/com/prog1/kepnezegeto/Main.java
@@ -3,6 +3,6 @@ package com.prog1.kepnezegeto;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello  hello");
+        System.out.println("asdasd  hello");
     }
 }
