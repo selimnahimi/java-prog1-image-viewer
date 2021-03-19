4837f79b45f82cf1f57c70d5247f8b08fd07bc86
HUNcamper <selimnahimi@gmail.com>
2021-03-19 11:32:26 +0100

    Update Main.java

--- src/com/prog1/kepnezegeto/Main.java
+++ src/com/prog1/kepnezegeto/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello hello hello");
     }
+
+    public void hello() {
+        System.out.println("Heloooooooo");
+    }
 }
