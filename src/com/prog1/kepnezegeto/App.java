3823de8d3bc7a5447d0a5d058b1bddd88aae4604
LikeFall123 <56880972+LikeFall123@users.noreply.github.com>
2021-03-26 17:14:41 +0100

    Add files via upload

---
+++ src/com/prog1/kepnezegeto/App.java
@@ -0,0 +1,71 @@
+package com.prog1.kepnezegeto;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class App{
+
+    private JButton button1;
+    private JPanel panel1;
+    private JLabel label1;
+    private JLabel image;
+
+    private final JFileChooser openFileChooser; //ez az ablak lesz a file valaszto
+    private BufferedImage originalImage; //ezt a kepet toltjuk be
+
+    public App() {
+        openFileChooser = new JFileChooser(); //erteket adunk a file valasztonak
+        openFileChooser.setCurrentDirectory(new File(System.getProperty("user.dir"))); //beallitjuk az alapvetk konyvtarat
+
+        openFileChooser.setFileFilter(new FileNameExtensionFilter("BMP images", "bmp"));
+        openFileChooser.setFileFilter(new FileNameExtensionFilter("PNG images", "png")); //ide irjuk milyen filetipusok lehetnek
+        openFileChooser.setFileFilter(new FileNameExtensionFilter("JPG images", "jpg"));
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnValue = openFileChooser.showOpenDialog(button1);
+
+                if(returnValue == JFileChooser.APPROVE_OPTION){ //meg lett e nyiva file vagy nem
+                    try{
+                        originalImage = ImageIO.read(openFileChooser.getSelectedFile()); //beolvassuk a kepet
+                        label1.setText("Image file successfully loaded!");
+
+                        //csinalunk egy uj labelt, es atmeretezzuk a kepet, majd rarakjuk arra
+
+                        BufferedImage resizedImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+                        Graphics2D g = resizedImage.createGraphics();
+                        g.drawImage(originalImage, 0, 0, 500, 500, null);
+                        g.dispose();
+                        image.setIcon(new ImageIcon(resizedImage));
+
+
+
+                    }catch (IOException ioe){
+                        label1.setText("No file choosen!");
+                    }
+                }
+                else{
+                    label1.setText("No file choosen");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(500,500);
+    }
+
+}
