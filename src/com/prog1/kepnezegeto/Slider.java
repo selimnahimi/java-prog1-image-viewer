08df008ebf7b3c605d8e3cf6461be46b04d81a32
LikeFall123 <56880972+LikeFall123@users.noreply.github.com>
2021-04-19 19:27:45 +0200

    slidervalues

--- src/com/prog1/kepnezegeto/Slider.java
+++ src/com/prog1/kepnezegeto/Slider.java
@@ -1,8 +1,11 @@
 package com.prog1.kepnezegeto;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 
 public class Slider extends JFrame{
@@ -15,7 +18,20 @@ public class Slider extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
-        setSize(300,100);
+        setSize(320,140);
+
+        Dictionary<Integer, Component> labelTable = new Hashtable<Integer, Component>();
+        labelTable.put(0, new JLabel("0"));
+        labelTable.put(45, new JLabel("45"));
+        labelTable.put(90, new JLabel("90"));
+        labelTable.put(135, new JLabel("135"));
+        labelTable.put(180, new JLabel("180"));
+        labelTable.put(225, new JLabel("225"));
+        labelTable.put(270, new JLabel("270"));
+        labelTable.put(315, new JLabel("315"));
+        labelTable.put(360, new JLabel("360"));
+
+        sliderValue.setLabelTable(labelTable);
 
         buttonSelect.addActionListener(new ActionListener() {
             @Override
